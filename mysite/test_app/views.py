c2c14e94ed0d06b84bc13acb195f62877750e0a7
Sreekar-reddy07 <sreekarreddykarnati@gmail.com>
2025-02-12 17:14:10 -0500

    made the website a bit more fancy

--- mysite/test_app/views.py
+++ mysite/test_app/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 def home(request):
-    return HttpResponse("<h1>Welcome to My Django Website!</h1>")
     return render(request, 'test_app/home.html')
